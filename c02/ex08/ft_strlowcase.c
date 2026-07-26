6ab7dc4f5dd22d49d32b08d82bdfd29e51165265
alaemaghchich <alaemaghchich2004@gmail.com>
2026-07-27 00:50:56 +0200

    c02

---
+++ c02/ex08/ft_strlowcase.c
@@ -0,0 +1,17 @@
+#include<unistd.h>
+
+char *ft_strlowcase(char *str){
+    int i = 0;
+    while(str[i])
+    {
+        if(str[i] >= 'A' && str[i] <= 'Z'){
+            str[i] += 32;
+        }
+        i++;
+    }
+    return str;
+}
+int main (){
+    char str[] = "HELLO  world";
+    ft_strlowcase(str);
+}
