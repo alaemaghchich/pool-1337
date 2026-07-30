df71aa85fbb83cd2d1fb434f99afdffb65d97078
alaemaghchich <alaemaghchich2004@gmail.com>
2026-07-31 00:14:22 +0200

    push c03

---
+++ c03/ex04/ft_strstr.c
@@ -0,0 +1,27 @@
+#include<unistd.h>
+
+char *ft_strstr(char *str, char *to_find){
+
+if (to_find[0] == '\0'){
+    return str;
+}
+
+int j;
+int i = 0;
+while(str[i]){
+    j = 0;
+    while(str[j+i] && str[j+i] == to_find[j]){
+        j++;
+    }
+    if(to_find[j] == '\0'){
+        return &str[i];
+    }
+    i++;
+}
+return NULL;
+}
+int main(){
+    char str[] = "hello world im alae";
+    char *p  = ft_strstr(str, "alae");
+    printf("%s" , p);
+}
